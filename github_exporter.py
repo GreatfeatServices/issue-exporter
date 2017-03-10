386e2221c6b1825dc994cbe1081afde52db02f5e
Ariel Silva Jr <ceosilvajr@gmail.com>
2017-03-10 11:39:38 +0800

    Update github_exporter.py

--- github_exporter.py
+++ github_exporter.py
@@ -1,20 +1,3 @@
-"""
-This is strongly based on https://gist.github.com/unbracketed/3380407;
-thanks to @unbracketed and the various commenters on the page.
-
-I've mainly cleaned up the code into basic methods, and included the
-various suggestions in the comments. Hope this is useful to someone.
-
-Make sure you have `requests` and `csv` installed via pip then run it:
-`python issue-exporter.py`
-
----
-
-Exports Issues from a specified repository to a CSV file
-Uses basic authentication (Github username + password) or token to retrieve Issues
-from a repository that username has access to. Supports Github API v3.
-"""
-
 import csv
 import requests
 
